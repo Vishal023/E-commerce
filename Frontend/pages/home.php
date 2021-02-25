168014354095187a9c8d9fb50c5fbdc462b5e527
Vishal Singh <42888189+Vishal023@users.noreply.github.com>
2021-02-25 16:57:31 +0530

    Modified home page with caraousel

--- Frontend/pages/home.php
+++ Frontend/pages/home.php
@@ -1,4 +1,36 @@
 <?php
 @include('./pages/nav.php');
 ?>
-
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="https://source.unsplash.com/1920x350/?electronic,gadget" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Best Electronic Gadgets</h3>
+                <p>A change you can see.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="https://source.unsplash.com/1920x350/?laptop,mobile" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Top Offers On Mobile</h3>
+                <p>An exciting place for the whole family to shop.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="https://source.unsplash.com/1920x350/?digital" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Digital Daily Deals</h3>
+                <p>Enrich your shopping list wisely.</p>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
