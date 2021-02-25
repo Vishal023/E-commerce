912ccbc03e39cff70d00a6a77ceba1f592b4f808
Vishal Singh <42888189+Vishal023@users.noreply.github.com>
2021-02-25 16:09:30 +0530

    added navigation bar

--- Frontend/pages/nav.php
+++ Frontend/pages/nav.php
@@ -1,3 +1,18 @@
-<nav>
-    <h1>Testing My NavBar</h1>
+<nav class="flex jc-sb ai-c">
+    <div class="Nav-bar flex jc-c ai-c">
+        <div class="Nav-items Nav-brand">
+            E-commerce
+        </div>
+        <div class="Nav-items Nav-search">
+            <input type="search" placeholder="Search for products, brands and more">
+        </div>
+    </div>
+    <div class="Nav-bar flex jc-fe ai-c">
+        <div class="Nav-items">
+            <p>Vishal</p>
+        </div>
+        <div class="Nav-items">
+            <p>🛒</p>
+        </div>
+    </div>
 </nav>
