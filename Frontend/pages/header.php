5bc1b5d4feb0894f66a5f4261fa7e51f965b92ea
Vishal Singh <42888189+Vishal023@users.noreply.github.com>
2021-03-09 10:04:54 +0530

    Added Profile Page

--- Frontend/pages/header.php
+++ Frontend/pages/header.php
@@ -10,11 +10,14 @@
         Here is the link for every stylesheets
         Add any css files below index.css
     -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./css/nav.css">
     <link rel="stylesheet" href="./css/login.css">
     <link rel="stylesheet" href="./css/home.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/profile.css">
+
+    <script src="https://kit.fontawesome.com/c794455c4b.js" crossorigin="anonymous"></script>
 
     <title>E-Commerce</title>
 </head>
---
+++ Frontend/pages/profile.php
@@ -0,0 +1,106 @@
+<section class="Profile flex wrap">
+    <aside class="Profile-left flex flex-col">
+        <div class="User flex ai-c">
+            <p>
+                <span>
+                    <i class="fas fa-user-circle"></i>
+                </span>
+                <span>
+                    Vishal Singh
+                </span>
+            </p>
+        </div>
+        <div class="User-nav flex flex-col">
+            <div class="User-navItems">
+                <button class="flex">
+                    <span>
+                        <i class="fas fa-shopping-bag"></i>
+                    </span>
+                    <span>
+                        My Orders
+                    </span>
+                </button>
+            </div>
+            <div class="User-navItems">
+                <button class="flex">
+                    <span>
+                        <i class="fas fa-tools"></i>
+                    </span>
+                    <span>
+                        Account Settings
+                    </span>
+                </button>
+            </div>
+            <div class="User-navItems">
+                <button class="flex">
+                    <span>
+                        <i class="fas fa-wallet"></i>
+                    </span>
+                    <span>
+                        Payments
+                    </span>
+                </button>
+            </div>
+            <div class="User-navItems">
+                <button class="flex btn-logout">
+                    <span>
+                        <i class="fas fa-sign-out-alt"></i>
+                    </span>
+                    <span>
+                        Logout
+                    </span>
+                </button>
+            </div>
+        </div>
+    </aside>
+    <aside class="Profile-right flex">
+        <div class="Personal-info">
+            <div class="Head flex jc-sb ai-c">
+                <h3>Personal Information</h3>
+                <button class="edit">Change</button>
+            </div>
+            <div class="User-detail flex flex-col">
+                <div>
+                    <input type="text" disabled value="Vishal" id="fname">
+                    <input type="text" disabled value="Singh" id="lname">
+
+                </div>
+                <div class="flex ai-c">
+                    <input disabled  type="radio" checked id="male" name="gender" value="male">
+                    <p>Male</p>
+                    <input disabled type="radio" id="female" name="gender" value="female">
+                    <p>Female</p>
+                    <input disabled type="radio" id="other" name="gender" value="other">
+                    <p>Other</p>
+                </div>
+            </div>
+            <div class="Head flex jc-sb ai-c">
+                <h3>Email Address</h3>
+                <button class="edit">Change</button>
+            </div>
+            <div class="User-detail">
+                <div>
+                    <input type="email" disabled value="name@mail.com">
+                </div>
+            </div>
+            <div class="Head flex jc-sb ai-c">
+                <h3>Mobile Number</h3>
+                <button class="edit">Change</button>
+            </div>
+            <div class="User-detail">
+                <div>
+                    <input type="number" disabled value="9786451238">
+                </div>
+            </div>
+            <div class="Head flex jc-sb ai-c">
+                <h3>Manage Address</h3>
+                <button class="edit">Change</button>
+            </div>
+            <div class="User-detail">
+                <div>
+                    <textarea disabled cols="10" rows="1">BH4, Lovely Professional University, Lovely Professional University, Kapurthala District, Punjab - 144411</textarea>
+                </div>
+            </div>
+        </div>
+    </aside>
+</section>
