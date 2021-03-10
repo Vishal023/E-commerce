d0fda3df6f1d2769f3aa947faadcc2b408cb949a
Vishal Singh <42888189+Vishal023@users.noreply.github.com>
2021-03-10 10:35:46 +0530

    intital commit to search result page

--- Frontend/index.php
+++ Frontend/index.php
@@ -3,7 +3,7 @@
 @include ('./pages/header.php');
 
 
-@include ('./pages/order.php');
+@include ('./pages/search.php');
 
 
 @include ('./pages/footer.php');
---
+++ Frontend/pages/search.php
@@ -0,0 +1,137 @@
+<?php
+$bought = [
+    array(
+        "name" => "Acer Predator Helios 300",
+        "color" => "Abyssal Black",
+        "seller" => "RetailNet",
+        "price" => 119990,
+        "dateOfDelivery" => "Jan 25, 2020",
+        "status" => true
+    ),
+    [
+        "name" => "Phone Cover",
+        "color" => "Black",
+        "seller" => "XYZ",
+        "price" => 190,
+        "dateOfDelivery" => "Mar 25, 2018",
+        "status" => true
+    ],
+    [
+        "name" => "Wrist Watch",
+        "color" => "Red Black",
+        "seller" => "Abyssal",
+        "price" => 500,
+        "dateOfDelivery" => false,
+        "status" => false
+    ], [
+        "name" => "Wrist Watch",
+        "color" => "Red Black",
+        "seller" => "Abyssal",
+        "price" => 500,
+        "dateOfDelivery" => false,
+        "status" => false
+    ], [
+        "name" => "Wrist Watch",
+        "color" => "Red Black",
+        "seller" => "Abyssal",
+        "price" => 500,
+        "dateOfDelivery" => false,
+        "status" => false
+    ], [
+        "name" => "Wrist Watch",
+        "color" => "Red Black",
+        "seller" => "Abyssal",
+        "price" => 500,
+        "dateOfDelivery" => false,
+        "status" => false
+    ], [
+        "name" => "Wrist Watch",
+        "color" => "Red Black",
+        "seller" => "Abyssal",
+        "price" => 500,
+        "dateOfDelivery" => false,
+        "status" => false
+    ], [
+        "name" => "Wrist Watch",
+        "color" => "Red Black",
+        "seller" => "Abyssal",
+        "price" => 500,
+        "dateOfDelivery" => false,
+        "status" => false
+    ]
+];
+?>
+
+<section class="Search flex wrap jc-c">
+    <aside class="Left">
+        <div class="Filter flex flex-col">
+            <div class="Head">
+                <h3>Filters</h3>
+            </div>
+            <div class="Category flex flex-col">
+                <h3>Price</h3>
+                <button class="btn btn-block btn-outline-secondary">
+                    Low to High
+                </button>
+                <button class="btn btn-block btn-outline-secondary">
+                    High to Low
+                </button>
+                <h3>Popularity</h3>
+                <button class="btn btn-block btn-outline-secondary">
+                    Most Popular
+                </button>
+                <button class="btn btn-block btn-outline-secondary">
+                    Least Popular
+                </button>
+                <h3>Other</h3>
+                <button class="btn btn-block btn-outline-secondary">
+                    Newest First
+                </button>
+                <button class="btn btn-block btn-outline-secondary">
+                    Relevance
+                </button>
+            </div>
+        </div>
+    </aside>
+    <aside class="Result flex flex-col">
+        <?php
+        foreach ($bought as $item) {
+            ?>
+            <div class="Card flex jc-c wrap">
+                <div class="Card-sub d1 flex">
+                    <div class="Card-img">
+                        <img src="./assets/img/watch.jfif" alt="">
+                    </div>
+                </div>
+                <div class="Card-sub d2 flex flex-col jc-sa">
+                    <div class="Card-head">
+                        <?php echo $item["name"]?>
+                    </div>
+                    <div class="Card-details">
+                        Color : <?php echo $item["color"]?>
+                    </div>
+                    <div class="Card-details">
+                        Seller : <?php echo $item["seller"]?>
+                    </div>
+                </div>
+                <div class="Card-sub d3 flex jc-c ai-c">
+                    $<?php echo $item["price"]?>
+                </div>
+                <div class="Card-sub d4 flex flex-col ai-c jc-sa">
+                    <div>
+                        <button class="btn btn-block btn-success " id="add">
+                            + Add
+                        </button>
+                    </div>
+                    <div>
+                        <button class="btn btn-block btn-danger disabled" id="rem">
+                            - Remove
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+    </aside>
+</section>
