06767c33b80721eb4ef83baa0d77d3acd0023544
Vishal Singh <42888189+Vishal023@users.noreply.github.com>
2021-03-16 14:19:38 +0530

    backend

---
+++ Backend/config.php
@@ -0,0 +1,16 @@
+<?php
+//
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "e-commerce";
+//
+//$db = mysqli_connect($servername, $username, $password, $dbname);
+//
+//if (!$db){
+//    die("Connection failed: " . mysqli_connect_error());
+//}else{
+//    echo "Success";
+//}
+//
+//?>
---
+++ Backend/session.php
@@ -0,0 +1,19 @@
+<?php
+//
+//include ('config.php');
+//
+//session_start();
+//
+//$user_check = $_SESSION['login_user'];
+//
+//$ses_sql = mysql_query($db,"select email from user where email = '$user_check' ");
+//
+//$row = mysql_fetch_array($ses_sql,MYSQL_ASSOC);
+//
+//$login_session = $row['email'];
+//
+//if(!isset($_SESSION['login_user'])){
+//    header("location:login.php");
+//    die();
+//}
+//?>
